49a9d0fd2da3c94ed363f24e1d8def80d4cdc2eb
AyaG96 <aya.gamal.zaher@gmail.com>
2024-01-24 08:45:30 -0800

    added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """class square"""
+    def __init__(self, size):
+        """constructor of the square"""
+        self.__size = size
